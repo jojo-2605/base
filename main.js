882b84a74736d806c06973574ff0ed3e339be983
jojo-2605 <jolanmayeur@gmail.com>
2026-08-24 11:26:16 +0200

    add js button

--- main.js
+++ main.js
@@ -1,1 +1,28 @@
-alert('test')
+// Install
+
+const installBtn = document.getElementById('install')
+let defferedPrompt = null
+
+window.addEventListener('beforeinstallprompt', event => {
+  event.preventDefault()
+  defferedPrompt = event
+  installBtn.classList.remove('hidden')
+  installBtn.addEventListener('click', installApp)
+})
+
+const installApp = () => {
+  defferedPrompt.prompt()
+  installBtn.classList.add('hidden')
+
+  // wait user choice
+  defferedPrompt.userChoice.then(choiceResult => {
+    console.log(choiceResult);
+    if( choiceResult.outcome === 'accepted') {
+      installApp.classList.add('hidden')
+    }
+    else{
+      console.log('PWA rejected')
+    }
+    defferedPrompt = null 
+  })
+}
