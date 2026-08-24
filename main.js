05f4c89f8c861a8ad92f9b675a2473476514f711
jojo-2605 <jolanmayeur@gmail.com>
2026-08-24 11:40:54 +0200

    add isPWA

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
+//mode actuel ?
+const isPWA = 
+       window.matchMedia("(display-mode: standalone)").matches || 
+       window.matchMedia("(display-mode: minimal-ui)").matches;
+
 // Install
 
 const installBtn = document.getElementById('install')
